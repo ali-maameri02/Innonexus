f32c288215478546664269da65544add08d0c4c8
ali_maameri02 <ali.maameri@univ-constanine2.com>
2024-09-03 16:26:33 +0100

    Moved contents of innoProject to root directory

--- inonexus_backend/Innonexus/Innonexus/settings.py
+++ inonexus_backend/Innonexus/Innonexus/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3ie9x1_ib7evgm-0@6ukrtt)4%@73a((k2^wz)8jv8-el07-91'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 CORS_ALLOWED_ORIGINS = [
